a8a3109919d420d8281d97ee25054052e00d9b49
Swapnil Patil <patilswapnil2149@gmail.com>
2023-03-31 12:48:28 +0530

    created partial ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,15 +13,21 @@ import{
   useContractCall,
 } from "@thirdweb-dev/react";
 import Loading from '../components/Loading';
+import { useState } from 'react';
 
 const Home: NextPage = () => {
   const address = useAddress();
-  const {contract, isLoading} = useContract(process.env.NEXT_PUBLIC_LOTTERY_CONTRACT_ADDRESS);
-  console.log(address);
+  const [quantity, setQuantity] = useState<number>(1);
+  const {contract, isLoading} = useContract(
+    process.env.NEXT_PUBLIC_LOTTERY_CONTRACT_ADDRESS
+    );
 
-  if(isLoading) return (<Loading/>);
+  const { data, remainingTickets } = useContractData(contract, "RemainingTickets")
+  }
   
 
+
+  if(isLoading) return (<Loading/>);
   if(!address) return (<Login />);
 
   return (
@@ -32,20 +38,85 @@ const Home: NextPage = () => {
 
       <Header/>
 
-        {/*Next Draw*/}
-        <div>
+        {/*Next Draw box*/}
+        <div className='space-y-5 md:space-y-0 m-5 md:flex md:flex-row 
+        items-start justify-center md:space-x-5'>
           <div className = 'stats-container'>
-            <h1 className='text-5xl text-white font-semibold text-center'>The NEXT DRAW</h1>
+            <h1 className='text-5xl text-white font-semibold text-center'>
+              The NEXT DRAW
+              </h1>
 
-            <div className='felx justify-between p-2 space-x-2'>
+            <div className='flex justify-between p-2 space-x-2'>
               <div className='stats'>
                 <h2 className='text-sm'>Total Pool</h2>
                 <p className='text-xl'>0.1 MATIC</p>
               </div>
+
+              <div className='stats'>
+                  <h2 className='text-sm'>Tickets Remaining</h2>
+                  <p className='text-xl'>100</p>
+              </div>
+            </div>
+
+              {/*Countdown counter*/}
+              {/*....*/}
+
+          </div>
+
+          <div className='stats-container space-y-2'>
+            <div>
+              <div className="stats-container">
+                <div className='flex justify-between items-center text-white pb-2'>
+                  <h2>Price per ticket</h2>
+                  <p>0.01 MATIC</p>
+                </div>
+
+                <div className='flex text-white items-center space-x-2 bg-[#091B18] border-[#004337] border p-4'>
+                  <p>TICKETS</p>
+                  <input 
+                    className='flex w-full bg-transparent text-right 
+                    outline-none' 
+                    type="number"
+                    min={1}
+                    max={10}
+                    value={quantity}
+                    onChange={e => setQuantity(Number(e.target.value))}
+                   />
+                </div>
+
+
+                <div className='space-y-2 mt-5'>
+                  <div className='flex items-center justify-between 
+                    text-emerald-300 text-sm italic font-extrabold'>
+                    <p>Total cost of tickets</p>
+                    <p>0.999</p>
+                  </div>
+
+                  <div>
+                    <div className='flex items-center justify-between 
+                    text-emerald-300 text-xs italic'>
+                      <p>Service fees</p>
+                      <p>0.001 MATIC</p>
+                    </div>
+
+                    <div className='flex items-center justify-between 
+                    text-emerald-300 text-xs italic'>
+                      <p>+ Network fees</p>
+                      <p>TBC</p>
+                    </div>
+                  </div>
+
+
+                  <button 
+                  className='mt-5 w-full bg-gradient-to-br
+                  from-orange-500 to-emerald-600 px-10 py-5 rounded-md
+                  text-white shadow-xl disabled:from-gray-600 disabled:text-gray-100
+                  disabled: to-gray-600 disabled:cursor-not-allowed'>Buy tickets</button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-
         {/*Price per ticket*/}
         <div>
 
